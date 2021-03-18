c528bddddf4e22830323518c619e0e30d3856ebd
Frank Tran <frank.tran@live.ca>
2021-03-17 20:56:53 -0700

    Adding Components

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from './../../models/Todo';
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  /** set the todos variable to a Todo model we defined */
+  todos:Todo[];
+
+  inputTodo:string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+        content: 'First ToDo',
+        completed: false
+      },
+      {
+        content: 'Second ToDo',
+        completed: true
+      }
+    ]
+  }
+
+  toggleDone (id:number):void {
+    this.todos.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+
+      return v;
+    })
+  }
+
+  deleteTodo (id:number) {
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }
+
+  addTodo () {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    });
+
+    this.inputTodo = ""
+
+  }
+
+}
